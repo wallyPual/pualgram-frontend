0024e87cbae684ea48ca5037f04a458733c138ca
paul <pub.wally@gmail.com>
2019-12-13 16:00:21 +0900

    client url

--- src/Apollo/Client.js
+++ src/Apollo/Client.js
@@ -2,12 +2,15 @@ import ApolloClient from 'apollo-boost';
 import { defaults, resolvers } from './LocalState';
 
 export default new ApolloClient({
-    uri: "http://localhost:4000",
-    clientState: {
-        defaults,
-        resolvers
-    },
-    headers: {
-      "Authorization": `Bearer ${localStorage.getItem("token")}`
-    }
-})
+  uri:
+    process.env.NODE_ENV === 'development'
+      ? 'http://localhost:4000'
+      : 'https://pualgram-backend.herokuapp.com/',
+  clientState: {
+    defaults,
+    resolvers
+  },
+  headers: {
+    Authorization: `Bearer ${localStorage.getItem('token')}`
+  }
+});
